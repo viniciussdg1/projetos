f0eb5284de0ca3a133a64d8e991a15085da40e09
viniciussdg1 <viniciussdg@hotmail.com>
2021-11-09 11:02:15 -0300

    Project Created

---
+++ AppDelivery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppDelivery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
